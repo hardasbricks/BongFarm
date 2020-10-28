a6dcd37313be9a9dd8567ef39655f71d2f7980d4
Dylan Moore <dylanmoore@Dylans-MBP.attlocal.net>
2020-10-28 15:16:07 -0500

    stuff:wq :wq

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+const replaceTemplate = require('./modules/replace-template');
+// const replaceTemplate = require('./modules/replace-template');
 ///////////////// FILE SYSTEMS /////////////////////////
 
 //blocking 
@@ -33,21 +35,19 @@ const url = require('url');
 
 
 //////////////////// SERVERS ///////////////////
+// const replaceTemplate = (temp, product) => {
+//     let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
+//     output = output.replace(/{%IMAGE%}/g, product.image);
+//     output = output.replace(/{%PRICE%}/g, product.price);
+//     output = output.replace(/{%FROM%}/g, product.from);
+//     output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+//     output = output.replace(/{%QUANTITY%}/g, product.quantity);
+//     output = output.replace(/{%DESCRIPTION%}/g, product.description);
+//     output = output.replace(/{%ID%}/g, product.id);
+//     if(!product.organic) { output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');}
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    if(!product.organic) { output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');}
-
-    return output;
-}
-
+//     return output;
+// }
 
 //html teplates
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
